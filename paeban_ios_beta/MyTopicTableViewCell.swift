ed90fbc2b801c0d93ec2e42ec48624c14e1c8167
Prototype.Sean <prototype.sean@gmail.com>
2016-10-14 19:10:37 +0800

    我的話題—父框按下反白，父框底漸層色

--- paeban_ios_beta/MyTopicTableViewCell.swift
+++ paeban_ios_beta/MyTopicTableViewCell.swift
@@ -18,13 +18,23 @@ class MyTopicTableViewCell: UITableViewCell {
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        self.selectionStyle = UITableViewCellSelectionStyle.none
     }
-
+    override public func setHighlighted(_ highlighted: Bool, animated: Bool) {
+        if highlighted {
+            
+        } else {
+            self.alpha = 1.0
+        }
+    }
+    
+    //MARK: 點父框字會亮
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
         // Configure the view for the selected state
+        // changing text color
+        self.topicTitle.textColor = selected ?  UIColor(red:0.97, green:0.44, blue:0.34, alpha:1.0) : UIColor(red:0.40, green:0.40, blue:0.40, alpha:1.0)
+        self.topicTitle.shadowColor = selected ?UIColor.white : UIColor.clear
+        self.topicTitle.shadowOffset = CGSize.init(width: 0, height: 0)
     }
-
 }
