9f5966cb8665a7e862823bb361c7c75d4b2f6f8f
Prototype.Sean <prototype.sean@gmail.com>
2016-11-07 15:39:25 +0800

    註冊頁面鍵盤抬VIEW

--- paeban_ios_beta/SettingViewController.swift
+++ paeban_ios_beta/SettingViewController.swift
@@ -10,12 +10,25 @@ import UIKit
 import Starscream
 
 class SettingViewController: UIViewController {
+    @IBOutlet weak var isTruePhotoSwitch: UISwitch!
 
+    @IBOutlet weak var isTruePhoto: UIImageView!
     @IBAction func logOutBtn(_ sender: AnyObject) {
         logOut()
     }
     @IBOutlet weak var pencilIcon: UIImageView!
-    
+    // 本人照片開關控制勾勾圖示出現與否
+    @IBAction func switchChanged(_ sender: AnyObject) {
+        if isTruePhotoSwitch.isOn {
+            print("on")
+            
+            isTruePhoto.tintColor = UIColor(red:0.00, green:0.73, blue:0.62, alpha:1.0)
+        }
+        else {
+            print("off")
+            isTruePhoto.tintColor = UIColor.clear
+        }
+    }
     // internal func
     func logOut(){
         cookie = nil
@@ -35,8 +48,6 @@ class SettingViewController: UIViewController {
             })
         }
     }
-    
-    
     // override
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,9 +55,9 @@ class SettingViewController: UIViewController {
         pencilIcon.tintColor = UIColor.orange
         pencilIcon.layer.shadowColor = UIColor.black.cgColor
         pencilIcon.layer.shadowOffset = CGSize(width:2,height:2)
-        
-        // Do any additional setup after loading the view.
+        // switch 原本大小是51x31 會往右下縮小
+        isTruePhotoSwitch.transform = CGAffineTransform(scaleX: 0.7, y: 0.7)
+        // 調整勾勾圖示的預設渲染模式
+        isTruePhoto.image = UIImage(named:"True_photo")?.withRenderingMode(UIImageRenderingMode.alwaysTemplate)
     }
-
-
 }
