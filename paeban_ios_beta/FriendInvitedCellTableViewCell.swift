7fdbd61c0a034e7141fb74d94450b5c476b823af
Prototype.Sean <prototype.sean@gmail.com>
2016-11-25 17:04:45 +0800

    好友跟好友邀請細胞建構

---
+++ paeban_ios_beta/FriendInvitedCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendInvitedCellTableViewCell.swift
+//  paeban_ios_beta
+//
+//  Created by 尚義 高 on 2016/11/25.
+//  Copyright © 2016年 尚義 高. All rights reserved.
+//
+
+import UIKit
+
+class FriendInvitedCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
