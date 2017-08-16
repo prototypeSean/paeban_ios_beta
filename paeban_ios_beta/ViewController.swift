8c92379fa5f3ee84ddd5ba60b585f48595c2458a
denniskao <prototype.sean@gmail.com>
2017-08-16 16:36:35 +0800

    同上次

--- paeban_ios_beta/ViewController.swift
+++ paeban_ios_beta/ViewController.swift
@@ -16,7 +16,7 @@ import CoreLocation
 // init config     --  named by DK
 public var my_blur_img_level_dic = [0:17, 1:12, 2:11, 3:10, 4:9, 5:8, 6:7, 7:5, 8:3, 9:0]
 public let version = "1.1.4.0"
-public let reset_database = true
+public let reset_database = false
 public let unlock_img_exp = 7
 public let local_host = "http://www.paeban.com/"
 // init config
@@ -340,7 +340,7 @@ public class ViewController: UIViewController, WebSocketDelegate, UITextFieldDel
         }
         let colorChangeAnimation = CABasicAnimation(keyPath: "colors")
         colorChangeAnimation.delegate = self
-        colorChangeAnimation.duration = 7.0
+        colorChangeAnimation.duration = 3.0
         colorChangeAnimation.toValue = colorSets[currentColorSet]
         colorChangeAnimation.fillMode = kCAFillModeForwards
         colorChangeAnimation.isRemovedOnCompletion = false
