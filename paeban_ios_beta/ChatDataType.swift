09ddbc0fb607429d9593f11fcc1f972c3848c29e
a110482 <a110482@yahoo.com.tw>
2018-03-08 09:15:49 +0800

    重構前

---
+++ paeban_ios_beta/ChatDataType.swift
@@ -0,0 +1,38 @@
+//
+//  ChatDataType.swift
+//  paeban_ios_beta
+//
+//  Created by elijah on 2018/3/8.
+//  Copyright © 2018年 尚義 高. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//// 重構 2018.3.8
+//struct Topic {
+//    var owner: String?
+//    var photo: UIImage?
+//    var title: String?
+//    var hashtags : [String]?
+//    var lastline : String
+//    var topicID : String
+//    var sex:String
+//    var isMe:Bool
+//    var online:Bool
+//    var ownerName:String
+//    var is_friend:Bool?
+//    var battery: Int?
+//    var distance:String?
+//}
+
+
+
+
+
+
+
+
+
+
+
